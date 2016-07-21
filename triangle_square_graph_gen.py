be3d669ef2a08b3a0524ad4def67bfc940f1fbc5
Jake <jsheradin@gmail.com>
2016-07-21 01:09:23 -0400

    More useful plots

--- triangle_square_graph_gen.py
+++ triangle_square_graph_gen.py
@@ -1,13 +1,38 @@
 #Generate a graph to visualize a pattern in triangle_square numbers
-
 import matplotlib.pyplot as plt
 
+#Load numbers from triangle_squares.txt
 nums = [line.rstrip('\n') for line in open("triangle_squares.txt", "r")]
 nums = list(map(int, nums))
-nums = nums[:150] #Really big bumers break matplotlib :(
 
+#Find last digit of numbers
+last_digit = []
+for n in range(len(nums)):
+    last_digit.append(nums[n]%10)
+    
+##Plot stuff
+#Occurrence
+nums = nums[:150] #Really big bumers break matplotlib :(
+plt.figure(1)
 plt.plot(nums)
+plt.title('Occurrence')
 plt.yscale('log')
+plt.ylabel('Value')
 plt.xscale('linear')
+plt.xlabel('nth Number')
 plt.grid(True)
-plt.show()
+plt.savefig('triangle_squares_graph_occurrence.png')
+
+#Ending digit
+plt.figure(2)
+plt.hist(last_digit, bins=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], normed=1)
+axes = plt.gca()
+axes.set_xlim([0, 10])
+plt.grid(True)
+plt.title('Terminating Digit')
+plt.ylabel('Probability')
+plt.xlabel('Digit')
+plt.savefig('triangle_squares_graph_terminating_digit.png')
+
+print("done")
+#plt.show()
--- triangle_square_solved.py
+++ triangle_square_solved.py
@@ -1,9 +1,14 @@
 #Algebraic solution to generate triangle-square numbers
 
 nums = []
+a = (1 + 2**0.5)
+b = (1 - 2**0.5)
+c = (4*2**0.5)
 for n in range(0, 200):
     #Math used from https://oeis.org/A001110
-    nums.append(round(((1 + 2**0.5)**(2*n) - (1 - 2**0.5)**(2*n))/(4*2**0.5))**2)
+    two_n = 2*n
+    nums.append(round(((a**two_n - b**two_n)/c)**2))
+    #print(n, ":", nums[n])
 with open("triangle_squares.txt", "w") as save_file:
     for item in nums:
         save_file.write("%s\n" % item)
