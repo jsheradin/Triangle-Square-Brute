db2e6e36f305d877a8e4f86fd9f2e1f62464351a
Jake <jsheradin@gmail.com>
2016-07-21 01:23:46 -0400

    Fixed OoO

--- triangle_square_solved.py
+++ triangle_square_solved.py
@@ -1,14 +1,13 @@
 #Algebraic solution to generate triangle-square numbers
 
 nums = []
-a = (1 + 2**0.5)
-b = (1 - 2**0.5)
-c = (4*2**0.5)
+a = (1 + (2**0.5))
+b = (1 - (2**0.5))
+c = (4 * (2**0.5))
 for n in range(0, 200):
     #Math used from https://oeis.org/A001110
     two_n = 2*n
     nums.append(round(((a**two_n - b**two_n)/c)**2))
-    #print(n, ":", nums[n])
 with open("triangle_squares.txt", "w") as save_file:
     for item in nums:
         save_file.write("%s\n" % item)
